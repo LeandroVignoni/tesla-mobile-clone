0dddc12b13beca24f6679b462cdcbe59f1cb59b6
LVignoni <leandrovignoni@gmail.com>
2021-03-09 09:42:08 -0300

    car item + style

---
+++ components/CarItem/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import StyledButton from "../StyledButton";
+import styles from "./styles"
+
+
+const CarItem = () => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelS.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
+      </View>
+      <StyledButton type="primary" content="Custom Order" onPress={() => {
+          console.warn("Custom order was pressed")
+      }}/>
+      <StyledButton type="secondary" content="Existing Inventory" onPress={() => {
+          console.warn("Existing inventory was pressed")
+      }}/>
+    </View>
+  );
+};
+
+export default CarItem;
